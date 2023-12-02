05f03a92fb08eb94f9c119c098a542d52e708d6c
muriloapolo <massps2@gmail.com>
2023-12-02 19:15:41 -0300

    Várias mudanças

--- js/app.js
+++ js/app.js
@@ -1,32 +1,4 @@
-﻿const displayResultado = document.querySelector('#resultado');
-const copyClipboard = document.querySelector('#copyClipboard');
-let spanCopy = document.querySelector('.alertCopy');
-
-// let info2 = '140889978976320172409063012510028544207E'
-//Remover espaços de uma string
-//.replace(/\s/g, '')
-const inputeCodigo = document.querySelector('#codeBar');
-
-inputeCodigo.addEventListener('keyup', getCodeToFormat, false);
-copyClipboard.addEventListener('click', addToClipBoard)
-
-
-function getCodeToFormat(e) {
-
-    let code = e.target.value;
-
-    if (e.which == 13) {
-        if (code.length < 40 || code.length > 40) return false
-        code = code.replace(/\s/g, '').trim()
-        getInfo(code)
-    }
-
-}
-
-
-
-
-const barCode = {
+﻿const barCode = {
     //dve  125
     '173537': 'DVE 20*25 125F',
     '173538': 'DVE 25*30 125F',
@@ -47,6 +19,79 @@ const barCode = {
 
 }
 
+// let info2 = '140889978976320172409063012510028544207E'
+//Remover espaços de uma string
+//.replace(/\s/g, '')
+
+class PageItens {
+    constructor() {
+        this.displayResultado = document.getElementById('resultado');
+        this.inputeCodigo = document.getElementById('codeBar');
+        this.copyClipboard = document.getElementById('copyClipboard');
+        this.spanCopy = document.getElementById('alertCopy');
+        this.errorSpan = document.getElementById('infoCodeError')
+        this.changeBg = document.getElementById('changeBg');
+    }
+
+
+    // Gera arquivos de primeira instância
+    imputCodeDocument() {
+        return this.inputeCodigo;
+    }
+    getCode() {
+        return this.inputeCodigo.addEventListener('keyup', getCodeToFormat);
+    }
+    showResult() {
+        return this.displayResultado;
+    }
+    copyToClip() {
+        return this.copyClipboard.addEventListener('click', addToClipBoard);
+    }
+    // Arquivos de segunda Instância
+    showHideSpan() {
+        return this.spanCopy;
+    }
+    errorSpan() {
+        return this.errorSpan;
+    }
+    changeBackground() {
+        return this.changeBg.addEventListener('click', () => {
+            let body = document.querySelector('body');
+            body.classList.toggle("darkBgColor");
+            console.log('teste')
+        })
+    }
+
+
+
+
+}
+
+
+const newDocument = new PageItens()
+newDocument.getCode()
+newDocument.copyToClip()
+newDocument.changeBackground()
+
+
+
+function getCodeToFormat(e) {
+
+    let code = e.target.value;
+
+    if (e.which == 13) {
+        if (code.length < 40 || code.length > 40) {
+            showErrorLineCode()
+            return
+        }
+        code = code.replace(/\s/g, '').trim()
+        getInfo(code)
+    }
+
+}
+
+
+
 
 function getInfo(code) {
     let resultadoFinal;
@@ -63,8 +108,8 @@ function getInfo(code) {
 
     resultadoFinal = `${barCode[tipo]}:  ${barCode[produto]} LOTE: ${lote} VAL: ${validade}; `
 
-    displayResultado.innerHTML += resultadoFinal + "</br>";
-    inputeCodigo.value = ""
+    newDocument.showResult().innerHTML += resultadoFinal + "</br>";
+    newDocument.imputCodeDocument().value = ""
 }
 
 function validateValidade(data) {
@@ -84,6 +129,10 @@ function addToClipBoard() {
 }
 
 function showSpan() {
-    setTimeout(() => { spanCopy.style.display = 'block' }, 100);
-    setTimeout(() => { spanCopy.style.display = 'none' }, 800)
+    setTimeout(() => { newDocument.showHideSpan().style.display = 'block' }, 100);
+    setTimeout(() => { newDocument.showHideSpan().style.display = 'none' }, 800)
+}
+function showErrorLineCode() {
+    setTimeout(() => { newDocument.errorSpan.style.display = 'block' }, 100);
+    setTimeout(() => { newDocument.errorSpan.style.display = 'none' }, 800)
 }
